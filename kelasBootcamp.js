53890a9a06d7dbc274f1dc5132d35f0f7cbea83d
choiZalfaa98 <choirunnisazalfaa10@gmail.com>
2024-06-12 23:47:14 +0700

    add quiz.html and with KKM result and NOT KKM result

--- kelasBootcamp.js
+++ kelasBootcamp.js
@@ -1,4 +1,26 @@
 // Mulai Quiz Button
 function redirectToPage() {
   window.location.href = 'quiz.html';
+  window.scroll(0,0);
+}
+
+// Kembali ke Materi Button
+function redirectToPage2() {
+  window.location.href = 'materi.html';
+  window.scroll(0,0);
+}
+
+// Melihat Hasil Skor
+function seeScore(targetedID, attributeName, attributeValue) {
+  // Get the target element
+  const targetElement = document.querySelector(targetedID);
+  
+  // Check if the target element exists
+  if (targetElement) {
+    // Set the specified attribute and value
+    targetElement.setAttribute(attributeName, attributeValue);
+    
+    // Scroll to the target element
+    targetElement.scrollIntoView({ behavior: 'smooth' });
+  }
 }
