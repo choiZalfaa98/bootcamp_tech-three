28d5156f2067c2b3442d8bdc9e169c9cfbaa39e9
Inayahputeri09 <inayahputeri09@gmail.com>
2024-06-13 12:21:23 +0700

    Add files via upload

--- config.php
+++ config.php
@@ -3,12 +3,5 @@ $host = "localhost";
 $username = "root"; 
 $password = ""; 
 $database = "techthree"; 
-
-// Membuat koneksi
-$conn = new mysqli($host, $username, $password, $database);
-
-// Memeriksa koneksi
-if ($conn->connect_error) {
-    die("Koneksi gagal: " . $conn->connect_error);
-}
+$conn = mysqli_connect ($host, $username, $password, $database);
 ?>
